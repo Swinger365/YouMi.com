86644e1a6a1804ac6ec2e752b542c3e6b7ca9f92
Дамир Абдурахмонов <abdurahmonovdamir60@gmail.com>
2022-12-25 13:28:01 +0500

    Шапка третьей странички

---
+++ scripts/links.js
@@ -0,0 +1,13 @@
+const aboutUs = document.querySelector("#aboutUs");
+const chat = document.querySelector("#chat");
+const burger = document.querySelector("#burger");
+const psychologists = document.querySelector("#psychologists");
+aboutUs.addEventListener("click",(evt)=>{
+    window.location.href = "../pages/about us.html"
+});
+psychologists.addEventListener("click",(evt)=>{
+    window.location.href = "../pages/psychologists.html"
+});
+// chat.addEventListener("click", (evt)=>{
+//     window.locatio.href = "../pages/chat.html";
+// })
