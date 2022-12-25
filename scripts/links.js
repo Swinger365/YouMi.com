1d5048aca5c6e11b5c6d6f287fb2f278baeedf03
Дамир Абдурахмонов <abdurahmonovdamir60@gmail.com>
2022-12-25 22:47:34 +0500

    Тарифы добавлены и улучшен адаптив и меню входа

--- scripts/links.js
+++ scripts/links.js
@@ -1,8 +1,20 @@
+const hiddenLog = document.querySelector("#hiddenLog");
 const aboutUs = document.querySelector("#aboutUs");
 const chat = document.querySelector("#chat");
 const burger = document.querySelector("#burger");
+const rates = document.querySelector("#rates");
 const psychologists = document.querySelector("#psychologists");
 const hidden = document.querySelector("#hidden-menu");
+const personalAcc = document.querySelector("#personalAccount");
+function personalAccount(evt){
+    hiddenLog.classList.toggle("hidden");
+}
+rates.addEventListener("click",(evt)=>{
+    window.location.href = "../pages/rates.html"
+});
+personalAcc.addEventListener("click",(evt)=>{
+    personalAccount();;
+})
 burger.addEventListener("click",(evt)=>{
     hidden.classList.toggle("hidden");
 });
